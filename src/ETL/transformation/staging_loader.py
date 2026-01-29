1ec722ad184591e0b10ef7deb9b0461d725a28de
SaulSRod <saulsrod07@gmail.com>
2026-01-28 23:09:09 -0500

    Sped-up staging SQL

--- src/ETL/transformation/staging_loader.py
+++ src/ETL/transformation/staging_loader.py
@@ -43,20 +43,8 @@ def faster_to_sql(df_name: str, engine: Engine, df: pd.DataFrame, table_name: st
     except Exception as e:
         print(f"Error in staging_loader.py -> write_df_to_table: {e}")
 
-
-def write_df_to_table(df_name: str, engine: Engine, df: pd.DataFrame, table_name: str, if_exists: str = "append", chunksize: int = 10000,) -> int:
-    if df is None or df.empty:
-        print(f"[staging_loader] for {table_name} is empty")
-        return 0
-
-    with engine.begin() as conn:
-        print(f"[staging_loader] writing {df_name} to sql table -> {table_name}")
-        df.to_sql(table_name, conn, if_exists=if_exists, index=False, chunksize=chunksize, method="multi")
-
-    return len(df)
-
 # load staging hdma table faster by using memory instead of df.to_sql-------------
-def df_fast_loader(sample: bool = True,seed: int = 0,engine: Optional[Engine] = None,) -> None:
+def load_hdma_staging(sample: bool = True,seed: int = 0,engine: Optional[Engine] = None,) -> None:
     """
     Function that writes dataframe to memory and avoids using df.to_sql
     > Given that df.to_sql writes using single queries to database, time quickly adds up even when using chunksize.
@@ -96,43 +84,9 @@ def df_fast_loader(sample: bool = True,seed: int = 0,engine: Optional[Engine] = 
     except Exception as e:
         print(f"Error in staging_loader.py -> write_df_to_table: {e}")
 
-# load staging hdma table -------------
-def load_hdma_staging(sample: bool = True,seed: int = 0,engine: Optional[Engine] = None,) -> None:
-    if engine is None:
-        engine = create_engine("postgresql+psycopg2:///credit_risk")
-
-    accepted_df = clean_hdma_accepted(sample=sample,seed=seed,)
-    rejected_df = clean_hdma_rejected(sample=sample,seed=seed,)
-
-    print(f"[staging_loader] HDMA accepted rows: {len(accepted_df)}")
-    print(f"[staging_loader] HDMA rejected rows: {len(rejected_df)}")
-
-    #load accepted data
-    accepted_cols = ['activity_year', 'action_taken', 'preapproval', 'loan_purpose', 'loan_amount', 'loan_term', 'applicant_credit_score_type',
-        'co_applicant_credit_score_type' , 'loan_to_value_ratio', 'income', 'debt_to_income_ratio', 'derived_loan_product_type']
-    
-    accepted_stage = accepted_df[accepted_cols].copy()
-
-    try:
-        inserted_acc = write_df_to_table("HDMA_accepted", engine,accepted_stage,table_name="staging_accepted_hdma",if_exists="append", chunksize=100000)
-        print(f"[staging_loader] Inserted {inserted_acc} rows inside staging_accepted_hdma")
-    except Exception as e:
-        print(f"Error in staging_loader.py -> write_df_to_table: {e}")
-
-    #load rejected data
-    rejected_cols = ['activity_year', 'action_taken', 'preapproval', 'loan_purpose', 'loan_amount', 'loan_term', 'applicant_credit_score_type',
-        'co_applicant_credit_score_type', 'denial_reason_1', 'loan_to_value_ratio', 'income', 'debt_to_income_ratio', 'derived_loan_product_type',]
-    rejected_stage = rejected_df[rejected_cols].copy()
-    try:
-        print("writing df to sql table -> staging_rejected_hdma")
-        inserted_rejec = write_df_to_table("HDMA_rejected", engine,rejected_stage,table_name="staging_rejected_hdma",if_exists="append", chunksize=10000)
-        print(f"[staging_loader] Inserted {inserted_rejec} rows inside staging_rejected_hdma")
-    except Exception as e:
-        print(f"Error in staging_loader.py -> write_df_to_table: {e}")
-        
 if __name__ == "__main__":
     engine = create_engine("postgresql+psycopg2:///credit_risk", executemany_mode="values_plus_batch", insertmanyvalues_page_size=100000, executemany_batch_page_size=100000)
-    df_fast_loader(sample = False, engine = engine)
+    load_hdma_staging(sample = False, engine = engine)
 
     '''
     
