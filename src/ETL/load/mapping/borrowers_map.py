b7fc7b21e557cc4a34ce9b27c88319ed6d151f52
SaulSRod <saulsrod07@gmail.com>
2026-01-30 23:42:24 -0500

    Updated borrowers table

--- src/ETL/load/mapping/borrowers_map.py
+++ src/ETL/load/mapping/borrowers_map.py
@@ -5,18 +5,19 @@ def map_accepted_borrowers(engine: Engine) -> None:
     # debt_to_income_ratio staging is TEXT --> strip non-numeric chars and cast to numeric
     sql = text("""
         INSERT INTO Borrowers (
+            action_taken                    SMALLINT,
+            loan_purpose                    SMALLINT,
             preapproval                     SMALLINT,
             applicant_credit_score_type     SMALLINT,     
-            co_applicant_credit_score_type  SMALLINT,
-            derived_loan_product_type       TEXT,
-            loan_purpose                    SMALLINT 
+            derived_loan_product_type       TEXT
         )
         SELECT DISTINCT
+            vah.action_taken,
+            vah.loan_purpose,
             vah.preapproval,
             vah.applicant_credit_score_type,
-            vah.co_applicant_credit_score_type,
-            vah.derived_loan_product_type,
-            vah.loan_purpose
+            vah.derived_loan_product_type
+            
         FROM valid_accepted_hdma vah;
     """)
 
@@ -27,19 +28,20 @@ def map_accepted_borrowers(engine: Engine) -> None:
 def map_rejected_borrowers(engine: Engine) -> None:
     # debt_to_income_ratio staging is TEXT --> strip non-numeric chars and cast to numeric
     sql = text("""
-        INSERT INTO Borrowers (             
+        INSERT INTO Borrowers (
+            action_taken                    SMALLINT,
+            loan_purpose                    SMALLINT,
             preapproval                     SMALLINT,
             applicant_credit_score_type     SMALLINT,     
-            co_applicant_credit_score_type  SMALLINT,
-            derived_loan_product_type       TEXT,
-            loan_purpose                    SMALLINT 
+            derived_loan_product_type       TEXT
         )
         SELECT DISTINCT
+            vrh.action_taken,
+            vrh.loan_purpose,
             vrh.preapproval,
             vrh.applicant_credit_score_type,
-            vrh.co_applicant_credit_score_type,
-            vrh.derived_loan_product_type,
-            vrh.loan_purpose
+            vrh.derived_loan_product_type
+               
         FROM valid_rejected_hdma vrh;
     """)
 
